cad670288b01be85e94bba8687232abff9a6a12c
Blanca Hernandez <blamca1029@gmail.com>
2021-05-20 17:03:26 -0700

    added forEach and filter methods

--- to-do.js
+++ to-do.js
@@ -93,6 +93,37 @@ class TodoList {
       throw new ReferenceError(`invalid index: ${index}`)
     }
   }
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+  filter(callback) {
+    let selectedTodos = [];
+    this.forEach(todo => {
+      if (callback(todo)) {
+        selectedTodos.push(todo);
+      }
+    });
+    return selectedTodos;
+  }
 }
 
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
 
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+list.add(todo4)
+list.add(todo5)
+list.add(todo6)
+todo1.markDone();
+todo5.markDone();
+
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
+list.filter(todo => todo.isDone()).first();
